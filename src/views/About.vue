005760f5503c38cae4e21b06626097d273c608d4
rokugatsua <rokugatsua@outlook.co.id>
2020-12-17 16:01:36 +0700

    add about this project

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <h2> About me </h2>
+  <div class="flex flex-col p-4">
+    <header class="border-b-2 w-full pb-2">
+      <h1 class="text-xl"> About </h1>
+    </header>
+    <main class="my-4">
+      <p>
+        Hello my name is Jun, this a some blog post like website SPA. i create it with Vue.js 3, Vue Router, Vuex.
+        and for API, i use jsonplaceholder from <a href="https://jsonplaceholder.typicode.com">https://jsonplaceholder.typicode.com</a>.
+      </p>
+    </main>
+
+
+  </div>
 </template>
 
 <script>
